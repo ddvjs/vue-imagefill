005c7bd4fd5d6e82607a86f8554faa95920d6a75
HadiChan <861883474@qq.com>
2018-06-05 19:16:11 +0800

    b

--- dist/main.js
+++ dist/main.js
@@ -37,17 +37,32 @@ module.exports =
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
 /******/ 		}
 /******/ 	};
 /******/
 /******/ 	// define __esModule on exports
 /******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
 /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -96,9 +111,11 @@ __webpack_require__(/*! ./lib/style/vue-imagefill.css */ "./src/lib/style/vue-im
 
 exports.default = {
   install: function install(Vue) {
-    Object.keys(_lib2.default).forEach(function (key) {
-      Vue.directive(key, _lib2.default[key]);
-    });
+    for (var key in _lib2.default) {
+      if (_lib2.default.hasOwnProperty(key)) {
+        Vue.directive(key, _lib2.default[key]);
+      }
+    }
   }
 };
 
@@ -114,6 +131,9 @@ exports.default = {
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 var emptyLeft = '(\\s|^)';
 var emptyright = '(\\s|$)';
 
@@ -134,7 +154,7 @@ function removeClass(elements, className) {
   }
 }
 
-module.exports = {
+exports.default = {
   hasClass: hasClass,
   addClass: addClass,
   removeClass: removeClass
@@ -158,10 +178,15 @@ Object.defineProperty(exports, "__esModule", {
 
 var _event = __webpack_require__(/*! ./event.js */ "./src/lib/event.js");
 
+var _event2 = _interopRequireDefault(_event);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.default = {
   center: function center(el, binding) {
     var hCenter = binding.modifiers.full ? 'Hcenter-full' : 'Hcenter';
     var vCenter = binding.modifiers.full ? 'Vcenter-full' : 'Vcenter';
+
     if (binding.value) {
       el.src = binding.value;
     }
@@ -178,16 +203,17 @@ exports.default = {
       var pRatio = pw / ph;
 
       if (pRatio > ratio) {
-        (0, _event.removeClass)(el, hCenter);
-        (0, _event.addClass)(el, vCenter);
+        _event2.default.removeClass(el, hCenter);
+        _event2.default.addClass(el, vCenter);
       } else {
-        (0, _event.removeClass)(el, vCenter);
-        (0, _event.addClass)(el, hCenter);
+        _event2.default.removeClass(el, vCenter);
+        _event2.default.addClass(el, hCenter);
       }
     };
   },
   flex: function flex(el, binding) {
-    (0, _event.addClass)(el, 'flex-box');
+    _event2.default.addClass(el, 'flex-box');
+
     if (el.getElementsByClassName('expansion').length) return;
     var div = document.createElement('div');
     div.className = 'expansion';
